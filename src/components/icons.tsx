9101c541bc3babd3eeb08c930edcaa5fabb16200
Tobias Schwerdtfeger <this@tobias-schwerdtfeger.dev>
2022-01-19 22:01:46 +0100

    wip

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -15,7 +15,7 @@ function createSvgIcon(d: string): JSX.Element {
 
 export function SolidCheck(): JSX.Element {
   return createSvgIcon(
-    // svg icon data is originally licenced under the MIT License by Refactoring UI Inc (https://github.com/tailwindlabs/heroicons).
+    // svg icon data is originally licenced under the MIT License by Refactoring UI Inc (https://github.com/tailwindlabs/heroicons)
     'M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z',
   )
 }
